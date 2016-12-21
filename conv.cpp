8f4ed501f7019e356de69ef0304f5bb4246b2eb2
Adriana-Dinca <Adriana.Dinca@ro.ibm.com>
2016-12-21 15:11:03 +0200

    clean unused code and implement the download step from conversion from BT to USD

--- conv.cpp
+++ conv.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
+#include <fstream>
  
-#include <curl/curl.h>
 #include "json/json.h"
 #include "json/json-forwards.h"
 
@@ -17,64 +17,36 @@ int main(void)
 	Json::Value root;   // starts as "null"; will contain the root value ater parsing
 	Json::Reader reader;
 	const std::string currentprice_doc = "currentprice.json";
-	
-	//const std::string conf_doc = "test.json";
 
-  	/*bool parsingSuccessful = reader.parse( currentprice_doc, root, true );
-	if ( !parsingSuccessful )
-	{
-    	// report to the user the failure and their locations in the document.
-    	std::cout  << "Failed to parse configuration\n"
-              << reader.getFormattedErrorMessages();
-    	return -1;
+	int get = system ("wget http://api.coindesk.com/v1/bpi/currentprice.json");
+	if (get == -1) {
+		std::cout  << "Error wget currentprice.json \n";
+    	return get;
 	}
 
+	std::filebuf fb;
+	if (fb.open ("currentprice.json",std::ios::in))
+	{
+		std::istream is(&fb);
+
+		bool parsingSuccessful = reader.parse( is, root, true );
+		if ( !parsingSuccessful )
+		{
+    		std::cout  << "Failed to parse configuration\n"
+      	        << reader.getFormattedErrorMessages();
+    		return -1;
+		}
+		
+		fb.close();
+	}
 	const Json::Value plugins = root["time"];
+
 	std::cout << plugins.size();
 	for ( int index = 0; index < plugins.size(); ++index ) {
  		  std::cout << plugins[index].asString() << std::endl;
- 	}*/
+ 	}
 
-
-  CURL *curl_handle;
-
-  static const char *bodyfilename = "body.out";
-  FILE *bodyfile;
- 
-  curl_global_init(CURL_GLOBAL_ALL);
- 
-  // init the curl session  
-  curl_handle = curl_easy_init();
- 
-  // set URL to get 
-  curl_easy_setopt(curl_handle, CURLOPT_URL, "http://www.coindesk.com/calculator/");
- 
-  // no progress meter please  
-  curl_easy_setopt(curl_handle, CURLOPT_NOPROGRESS, 1L);
- 
-  // send all data to this function  
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_data);
- 
- 
-  // open the body file  
-  bodyfile = fopen(bodyfilename, "wb");
-  if(!bodyfile) 
-  {
-    curl_easy_cleanup(curl_handle);
-    return -1;
-  }
- 
-  // we want the body be written to this file handle instead of stdout  
-  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, bodyfile);
- 
-  // get it! 
-  curl_easy_perform(curl_handle);
- 
-  // close the body file 
-  fclose(bodyfile);
- 
-  // cleanup curl stuff 
-  curl_easy_cleanup(curl_handle);
+ 	std::cout << "after plugins\n";
  
   return 0;
 }
